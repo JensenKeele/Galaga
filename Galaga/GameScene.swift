168dc6ebfb575d2b8e65628cb7dc58908ca29c94
CCollins1016 <ccollins@bsd220.org>
2026-04-21 10:53:44 -0500

    created moving stars background

--- Galaga/GameScene.swift
+++ Galaga/GameScene.swift
@@ -14,6 +14,21 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-        
+        createBackground()
+    }
+    
+    func createBackground() {
+        let Stars = SKTexture( imageNamed: "Stars")
+        for i in 0...1 {
+            let StarsBackground = SKSpriteNode(texture: Stars)
+            StarsBackground.zPosition = -1
+            StarsBackground.position = CGPoint(x: 0, y: StarsBackground.size.height * CGFloat(i))
+            addChild(StarsBackground)
+            let moveDown = SKAction.moveBy(x: 0, y: -StarsBackground.size.height, duration: 20)
+            let moveReset = SKAction.moveBy(x: 0, y: StarsBackground.size.height, duration: 0)
+            let moveLoop = SKAction.sequence ([moveDown, moveReset])
+            let moveForever = SKAction.repeatForever (moveLoop)
+            StarsBackground.run(moveForever)
+        }
     }
 }
